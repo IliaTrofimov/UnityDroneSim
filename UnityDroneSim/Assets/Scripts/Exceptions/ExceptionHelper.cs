77e9d8679ed53b24b5944b767187ff81925d60ce
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2025-04-13 20:00:22 +0300

    Code formatting

--- UnityDroneSim/Assets/Scripts/Exceptions/ExceptionHelper.cs
+++ UnityDroneSim/Assets/Scripts/Exceptions/ExceptionHelper.cs
@@ -8,29 +8,38 @@ namespace Exceptions
         /// <summary>Throws <see cref="MissingComponentException"/> if parameter obj is null.</summary>
         public static void ThrowIfComponentIsMissing<TImport>(TImport obj, string componentName)
         {
-            if (obj == null) 
-                throw new MissingComponentException($"Missing component '{componentName}' of type {typeof(TImport).Name}.");
+            if (obj == null)
+                throw new MissingComponentException(
+                    $"Missing component '{componentName}' of type {typeof(TImport).Name}."
+                );
         }
-        
+
         /// <summary>Throws <see cref="MissingComponentException"/> if parameter obj is null.</summary>
-        public static void ThrowIfComponentIsMissing<TImport, TParent>(TParent parent, TImport obj, string componentName)
+        public static void ThrowIfComponentIsMissing<TImport, TParent>(
+            TParent parent,
+            TImport obj,
+            string componentName)
         {
-            if (obj == null) 
-                throw new MissingComponentException($"Parent object {typeof(TParent).Name} is missing component '{componentName}' of type {typeof(TImport).Name}.");
-        } 
-        
+            if (obj == null)
+                throw new MissingComponentException(
+                    $"Parent object {typeof(TParent).Name} is missing component '{componentName}' of type {typeof(TImport).Name}."
+                );
+        }
+
         /// <summary>Throws <see cref="MissingComponentException"/> if parameter obj is null.</summary>
         public static void ThrowIfComponentIsMissing<TImport>(TImport obj)
         {
-            if (obj == null) 
+            if (obj == null)
                 throw new MissingComponentException($"Missing component of type {typeof(TImport).Name}.");
         }
-        
+
         /// <summary>Throws <see cref="MissingComponentException"/> if parameter obj is null.</summary>
         public static void ThrowIfComponentIsMissing<TImport, TParent>(TParent parent, TImport obj)
         {
-            if (obj == null) 
-                throw new MissingComponentException($"Parent object {typeof(TParent).Name} is missing component of type {typeof(TImport).Name}.");
-        } 
+            if (obj == null)
+                throw new MissingComponentException(
+                    $"Parent object {typeof(TParent).Name} is missing component of type {typeof(TImport).Name}."
+                );
+        }
     }
 }
