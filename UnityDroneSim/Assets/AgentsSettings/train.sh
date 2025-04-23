ae258509caac42ddf9c95b70d0a42057473225dc
Ilia Trofimov <ilia.trofimov2001@icloud.com>
2025-04-23 23:21:53 +0300

    Improve train starter script
    Now using more parameters to start training. All useful ml-agents params are listed as script variables, so you can tweak them inside code and just run script.

--- UnityDroneSim/Assets/AgentsSettings/train.sh
+++ UnityDroneSim/Assets/AgentsSettings/train.sh
@@ -1,6 +1,12 @@
 CONDA_PATH=~/miniconda3/bin/activate
 CONDA_ENV='mlagents'
 DEFAULT_CONFIG=./drone_agents_config.yml
+
+WIDTH=640
+HEIGHT=480
+TIME_SCALE=1
+RUN_ID=""
+
 agent_config=$1
 
 if [ ! "$agent_config" ]; then
@@ -16,4 +22,11 @@ if [ ! -f "$agent_config" ]; then
 fi
 
 source $CONDA_PATH $CONDA_ENV
-mlagents-learn "$agent_config" --force
+
+if [ -z "$RUN_ID" ]; then
+  echo "Forcing new run ..." 
+  mlagents-learn "$agent_config" --force --time-scale "$TIME_SCALE" --width "$WIDTH" --height "$HEIGHT"
+else 
+  echo "Trying to resume run '$RUN_ID' ..." 
+  mlagents-learn "$agent_config" --resume --run-id "$RUN_ID" --time-scale "$TIME_SCALE" --width "$WIDTH" --height "$HEIGHT"
+fi
